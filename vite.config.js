5cda7999569e9fe794a0bdd93c80b26a26e4b4ff
Jeni Hales <hal22036@byui.edu>
2023-11-29 08:58:28 -0800

    fix build

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   root: "src/",
 
   build: {
-    outDir: "dist",
+    outDir: "../dist",
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
